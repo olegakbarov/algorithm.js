b31b59bf94361024b922a11a2d2a22d442276f9b
Oleg Akbarov <olegakbarov@gmail.com>
2016-03-18 01:09:05 +0300

    added tests

---
+++ tests/seq.js
@@ -0,0 +1,21 @@
+import test from 'tape';
+import { Seq } from '../lib/Seq';
+
+test('seq methods should work correctly', assert => {
+  let msg;
+
+  let instance = new Seq(['one', 'two', 'three']);
+  let first = instance.first()
+  let last = instance.last()
+  let nth = instance.nth(2)
+
+  msg = 'first() works properly';
+  assert.deepEqual(first, 'one', msg);
+
+  msg = 'last() works properly';
+  assert.deepEqual(last, 'three', msg);
+
+  msg = 'nth() works properly';
+  assert.deepEqual(nth, 'two', msg);
+  assert.end();
+});
