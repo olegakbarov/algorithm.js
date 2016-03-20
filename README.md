fa463aed7ea8f6156ad31eea8ddcff3f8433fe5b
Oleg Akbarov <olegakbarov@gmail.com>
2016-03-20 20:54:49 +0300

    roadmap in readme

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # Computer Science in JavaScript
 
 This repo is attempt to refresh my knowledge in CS basics by reimplementing basic algorithms and data scructures.
+
+### Project roadmap
+
+Here's the things i'd like to bring here. Though plans may change 😁
+
+#### Algorithms
+
+- [x] Merge sort
+- [ ] Bubble sort
+- [ ] Quicksort
+
+#### Data Structures
+
+- [x] List
+- [ ] Linked list
+- [ ] Seq
+- [ ] Stack
+- [ ] Set
+- [ ] Map
+
+#### Functional stuff
+
+- [ ] Curry
+- [ ] Compose
+- [ ] Memoize
+- [ ] Pick
+- [ ] Pluck
+- [ ] Partition
+- [ ] Lens
+- [ ] Transduce
--- src/list/list.js
+++ src/list/list.js
@@ -9,65 +9,75 @@ export class List {
    * @return {List}
    */
   constructor(items) {
-    this.data = items;
-    this._index = 0;
-  }
+    /**
+     * @private store
+     * @type {array}
+     */
+    let store = items;
 
-  /**
-   * @return {String} returns stringified representation
-   * of the content of list
-   */
-  toString() {
-    return this.data.toString();
-  }
+    /**
+     * we store current element index in this variable
+     * @private index
+     * @type {number}
+     */
+    let index = 0;
 
-  /**
-   * @return {Number} return index of current item
-   */
-  current() {
-    return this.data[this._index];
-  }
+    /**
+     * @return {String} returns stringified representation
+     * of the content of list
+     */
+    this.toString = () => {
+      return store.toString();
+    };
 
-  /**
-   * @return {Number}
-   */
-  next() {
-    return this.data[this._index++];
-  }
+    /**
+     * @return {Number} return index of current item
+     */
+    this.current = () => {
+      return store[index];
+    };
 
-  /**
-   * @param {any} el - element to be inserted
-   * @param {number} ix - index to append element to
-   */
-  insert(el, ix) {
-    this.data.splice(ix - 1, 1, el);
-  }
+    /**
+     * @return {Number}
+     */
+    this.next = () => {
+      return store[index++];
+    };
 
-  /**
-   * @param {any} el - finds and returns element if it exists in the list
-   * @return {Number} returns index of provided element
-   */
-  find(el) {
-    let ix = this.data.indexOf(el);
+    /**
+     * @param {any} el - element to be inserted
+     * @param {number} ix - index to append element to
+     */
+    this.insert = (el, ix) => {
+      store.splice(ix - 1, 1, el);
+    };
 
-    if (ix > 0) {
-      return this.data[ix];
-    } else {
-      return false;
-    }
-  }
+    /**
+     * @param {any} el - finds and returns element if it exists in the list
+     * @return {Number} returns index of provided element
+     */
+    this.find = el => {
+      let ix = store.indexOf(el);
 
-  /**
-   * @param {any} el - remove element if it exists in the list
-   * @return {Number} returns index of removed element
-   */
-  remove(el) {
-    let ix = this.data.indexOf(el);
+      if (ix > 0) {
+        return store[ix];
+      } else {
+        return false;
+      }
+    };
 
-    if (ix > 0) {
-      this.data.splice(ix, 1);
-    } else {
-      return false;
-    }
+    /**
+     * @param {any} el - remove element if it exists in the list
+     * @return {Number} returns index of removed element
+     */
+    this.remove = el => {
+      let ix = store.indexOf(el);
+
+      if (ix > 0) {
+        store.splice(ix, 1);
+      } else {
+        return false;
+      }
+    };
   }
 }
