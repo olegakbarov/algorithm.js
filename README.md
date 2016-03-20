4d388a0472e9a6d4113cf8212fe135d1898cc69f
Oleg Akbarov <olegakbarov@gmail.com>
2016-03-20 16:28:32 +0300

    esdoc + gh-pages deploy

---
+++ README.md
@@ -0,0 +1,3 @@
+# Computer Science in JavaScript
+
+This repo is attempt to refresh my knowledge in CS basics by reimplementing basic algorithms and data scructures.
--- tests/list.test.js
+++ tests/list.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-statements */
 import test from 'tape';
 import { List } from '../lib/list/list.js';
 
@@ -11,13 +12,17 @@ test('[list] Methods', assert => {
   list.next();
   assert.equal(list.current(), 2, msg);
 
-  msg = 'list index find() correct item';
-  list.remove(3);
-  assert.equal(list.toString(), '1,2,4,5', msg);
+  msg = 'find() returns correct item at index';
+  let four = list.find(4);
+  assert.equal(four, 4, msg);
+
+  msg = 'add() adds item';
+  list.add(6);
+  assert.equal(list.toString(), '1,2,3,4,5,6', msg);
 
   msg = 'list index changes accordingly to remove()';
   list.remove(3);
-  assert.equal(list.toString(), '1,2,4,5', msg);
+  assert.equal(list.toString(), '1,2,4,5,6', msg);
   assert.end();
 });
 
