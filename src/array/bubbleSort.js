1294f869f7b48b9557d9031071a6b5ad87c6bee7
Oleg Akbarov <olegakbarov@gmail.com>
2016-10-08 21:03:25 +0300

    + bubbleSort

--- src/array/bubbleSort.js
+++ src/array/bubbleSort.js
@@ -24,16 +24,19 @@ export function swap(arr, a, b) {
  */
 export function bubbleSort(arr) {
   let isSorted = false;
+  let lastUnsorted = arr.length - 1;
 
   while (!isSorted) {
     isSorted = true;
 
-    for (let i = 0; i < arr.length - 1; i++) {
+    for (let i = 0; i < lastUnsorted; i++) {
       if (arr[i] > arr[i + 1]) {
         swap(arr, i, i + 1);
         isSorted = false;
       }
     }
+
+    lastUnsorted--;
   }
 
   return arr;
